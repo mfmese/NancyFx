919e63a607847930e1a8ee6b81cda08191b4ef20
m.fethimese <m.fethimese@gmail.com>
2020-10-18 17:38:15 +0300

    Initial base line

---
+++ NancyNetFramework/MainModule.cs
@@ -0,0 +1,44 @@
+﻿using Nancy;
+using Nancy.Routing;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NancyNetFramework
+{
+    public class MainModule : NancyModule
+    {
+
+        public static long counter = 0;
+        public MainModule()
+        {         
+            Get("/", parameters =>
+            {
+                dynamic model = new ExpandoObject();
+                model.Name = "Test";
+                return View["MyView.cshtml", model];
+            });
+
+            Get("/greeting", args =>
+            {
+                var name = this.Request.Query["name"];
+                if (!name)
+                {
+                    name = "World";
+                }
+
+                dynamic greeting = new ExpandoObject();
+
+                greeting.Counter = ++counter;
+                greeting.Name = $"Hello, {name}";
+
+                return greeting.Counter + " " + greeting.Name;
+
+            });
+        }
+    }
+}
---
+++ NancyNetFramework/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NancyNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:8080";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Running on {0}", url);
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+            }
+        }
+
+        public class Startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                app.UseNancy();
+            }
+        }
+    }
+}
